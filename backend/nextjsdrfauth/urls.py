d6909334505f29f1ba3fe6226a14c0eb12960564
Jobin Abraham <jobin.abraham15@gmail.com>
2022-03-03 00:20:00 +0530

    Added urls

--- backend/nextjsdrfauth/urls.py
+++ backend/nextjsdrfauth/urls.py
@@ -2,5 +2,6 @@ from django.urls import path, include
 from .views import GoogleLoginView
 
 urlpatterns = [
+  path("accounts/", include('allauth.urls'), name='socialaccount_signup'),
   path("google/", GoogleLoginView.as_view(), name = "google"),
 ]
